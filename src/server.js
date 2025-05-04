93bfc496c7be07ae9db0f20cfb0896efec3c730f
TinKot <tin2545kot@gmail.com>
2025-05-04 11:06:56 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWebRoutes from './route/web';
+import initWebRoutes from './routes/web';
 require('dotenv').config();
 
 let app = express();
 
-//config app
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,7 +14,6 @@ viewEngine(app);
 initWebRoutes(app);
 
 let port = process.env.PORT || 6969;
-//Port === undefined => port = 6969
 
 app.listen(port, () => {
     //callback
